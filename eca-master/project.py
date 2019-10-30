f53b249ebc0fb2f76c8106dd127758fef693b915
andreioff <andreioff@yahoo.ro>
2019-10-30 10:29:41 +0100

    Add project files

---
+++ eca-master/project.py
@@ -0,0 +1,51 @@
+from eca import *
+
+import random
+
+root_content_path = 'project_static'
+
+from eca.generators import start_offline_tweets
+import datetime
+import textwrap
+import eca.http
+
+choice = 0
+
+def filter_english(data):
+    if data['lang'] == 'en':
+        return 1
+    return 0
+def filter_dutch(data):
+    if data['lang'] == 'nl':
+        return 1
+    return 0
+def filter_teams(data):
+    if "team" in data['text']:
+        return 1
+    return 0
+def filter_by(data):
+    print(choice)
+    if choice == 0:
+        return 1
+    elif choice == 1:
+        filter_english(data)
+    elif choice == 2:
+        filter_dutch(data)
+    elif choice == 3:
+        filter_teams(data)
+
+@event('init')
+def setup(ctx, e):
+   start_offline_tweets('bata_2014.txt')
+
+def add_request_handlers(httpd):
+    httpd.add_route('/api/filter', eca.http.GenerateEvent('filter'), methods=['POST'])
+
+@event('filter')
+def change_filter(ctx, e):
+    choice = e.data['value']
+
+@event('tweet')
+@condition(lambda c,e: filter_by(e.data))
+def echo(c,e):
+    emit('tweet', e.data)
